f4b0c80c9ee3d87fe851b1b09194da354c91e387
Daniel - Iulian Sabau <daniel.sabau@deventure.co>
2020-07-27 17:22:37 +0300

    Project setup

---
+++ Xml2Sb/Xml2Sb/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.IO;
+using System.Xml;
+
+namespace Xml2Sb
+{
+    class MainClass
+    {
+        private const string FILE_PATH = @"../../layout.xml";
+
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            var reader = new XmlTextReader(FILE_PATH);
+            while (reader.Read())
+            {
+                var name = reader.Name;
+            }
+        }
+    }
+}
